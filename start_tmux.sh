0c7ce4730f271952dd97480509ca74d3b22d7342
老馬 <laomafeima@gmail.com>
2018-03-20 10:22:56 +0800

    Update start_tmux.sh

--- start_tmux.sh
+++ start_tmux.sh
@@ -6,11 +6,11 @@ then
 
     tmux attach -t work
 else
-    tmux new-session -s python -n editor -d
-    tmux split-window -v -p 15 -t python:0.0
-    tmux new-window -n console -t python
-    tmux select-window -t python:0
-    tmux select-pane -t python:0.0
+    tmux new-session -s work -n editor -d
+    tmux split-window -v -p 15 -t work:0.0
+    tmux new-window -n console -t work
+    tmux select-window -t work:0
+    tmux select-pane -t work:0.0
     #tmux send-keys -t development 'cd ~/devproject' C-m
-    tmux attach -t python
+    tmux attach -t work
 fi
